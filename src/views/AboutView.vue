020bcccde81095ec3e2b8e9c42c87becd0a5ac91
Esther J. Iyege <faithesther20@gmail.com>
2024-05-02 10:34:04 +0100

    Ninja Jobs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique error, eveniet facilis modi earum exercitationem, molestiae alias commodi eos vitae rerum ex, asperiores at! Modi eligendi delectus inventore libero deserunt?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum quam doloribus corrupti odit eaque aspernatur laborum nihil, reiciendis qui, laudantium eius, dolor cumque placeat. Fuga a ab natus? Odit odio deleniti eaque! Rem, doloribus dolorum. Commodi cupiditate voluptate pariatur, inventore incidunt vitae ducimus doloribus sint alias dignissimos dolor architecto! Voluptatem.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Homepage</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, cum? Adipisci minima corrupti facilis sit molestiae accusantium voluptatibus aut quos exercitationem expedita, rem, omnis vel. Cumque non minima fugiat nobis alias consequatur repellat odio iusto perferendis voluptatibus iure obcaecati itaque, neque sint voluptate nulla ratione necessitatibus doloribus? Delectus numquam fuga quo, rerum mollitia perferendis, maxime aut molestias maiores laboriosam iure asperiores. Architecto numquam ex ea labore ad quia impedit nisi at tempora cupiditate? Minus, aliquid! Architecto est voluptatem ab, mollitia sint labore cupiditate eum consectetur eaque aliquid molestias voluptates possimus esse itaque magni soluta assumenda perferendis, veniam nihil sequi incidunt.</p>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore et nemo dignissimos excepturi cupiditate asperiores est dolorum molestias doloribus! Veniam labore molestias laboriosam optio assumenda rerum, omnis debitis. Sequi odit labore vitae, numquam totam corrupti debitis dolor a beatae atque! Nesciunt obcaecati numquam laboriosam, aliquid dolorum harum quod sint quo nulla debitis, cum sit nostrum, quae tempora. Optio explicabo totam error quae iure blanditiis recusandae nisi cupiditate animi et ex assumenda accusantium laboriosam, tempore veritatis provident pariatur? Aliquid eveniet autem nulla incidunt ipsam? Officia illo quasi nam aut quos id dignissimos quas, expedita doloribus iste vero quae at consectetur obcaecati!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
